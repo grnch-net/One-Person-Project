efafea0da0a232daba698d738e0598d3a95f8570
Eugene Gulevich <guleviche@digicode.net>
2018-02-25 08:56:53 +0200

    SceneModule: create interface file & default parameters & add dimension attr

--- src/ave/scene/scene-abstract.ts
+++ src/ave/scene/scene-abstract.ts
@@ -1,46 +1,48 @@
+import { Default, DimensionType } from "./../config";
 import { Animator } from "./../animator/animator";
 import { World } from "./../graphic/world";
 import { Camera } from "./camera";
 import { easyHTML } from "../../utils/easy-html";
 import { easyEvent } from "../../utils/easy-event";
 
-interface ISceneAbstract {
-	element: HTMLElement;
-	scene_width: number;
-	scene_height: number;
-	animator: Animator;
-	world: World;
-	mainCamera: Camera;
-
-	render(): void
-}
-
 export interface ISceneAbstractParameters {
 	width: number;
 	height: number;
 	nodeId?: string;
+	dimension?: DimensionType;
 }
 
 export abstract class SceneAbstract implements ISceneAbstract {
 	public element: HTMLElement;
-	public scene_width: number;
-	public scene_height: number;
 	public animator: Animator;
 	public mainCamera: Camera;
 	public world: World;
+	public scene_width: number = Default.scene.scene_width;
+	public scene_height: number = Default.scene.scene_height;
+
+	protected _dimension: DimensionType = Default.scene.dimension;
 
 	constructor(parameters:ISceneAbstractParameters) {
-		this.scene_width = parameters.width;
-        this.scene_height = parameters.height;
+		if (parameters.width)
+			this.scene_width = parameters.width;
+		if (parameters.height)
+	        this.scene_height = parameters.height;
+		if (parameters.dimension)
+			this._dimension = parameters.dimension;
 
 		if (parameters.nodeId)
-            this.element = this.initScene(parameters.nodeId);
-        else
-            this.element = this.createScene();
+			this.element = this.initScene(parameters.nodeId);
+		else
+			this.element = this.createScene();
 
 		this.animator = new Animator({ active: true });
 		this.mainCamera = new Camera(this);
 		this.createWorld();
+	}
+
+	get dimension(): DimensionType { return this._dimension; }
+	set dimension(value: DimensionType) {
+		this._dimension = value;
 	}
 
 	protected initScene(nodeId: string): HTMLElement {
@@ -56,7 +58,7 @@ export abstract class SceneAbstract implements ISceneAbstract {
 
 	protected createWorld(): void {
 		this.world = new World();
-	}
+		this.world.scene = this;	}
 
 	protected initEvents(): void {
 		easyEvent.addEvent(window, 'Resize', (e:any) => this.onResize(e) );
